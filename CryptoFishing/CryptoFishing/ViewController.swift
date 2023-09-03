b802da0716d772d08461da6211cc6c8b9c3fe219
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-03 18:48:26 +0300

    Added Table View Cell Contents

---
+++ CryptoFishing/CryptoFishing/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CryptoFishing
+//
+//  Created by Berat Rıdvan Asiltürk on 3.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Crypto Currency"
+        content.secondaryText = "Crypto Price"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
