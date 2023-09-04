2a7e42d93065a337506065d3b10b0cda59e991c9
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-04 15:09:52 +0300

    Done Decode Data Operations in WebServise Class

--- CryptoFishing/CryptoFishing/Service/WebSevice.swift
+++ CryptoFishing/CryptoFishing/Service/WebSevice.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+enum CryptoError : Error {
+    case serverError
+    case parsingError
+}
+
 class WebService {
     
     // Islem bittikten sonra ne yapilmasi gerektigini comletion handler sayesinde duzenleriz
@@ -14,11 +19,33 @@ class WebService {
     // Result yapisi bize completion edilecek seyin success or fail durumlarini degerlendirebilmemiz icin hazir bir iceriktir. [result'a tikla oku :)]
     // Result icerisinde ne dondurulecekse <> icerisine yazmamiz gerekiyor
     // Currency'i bir array seklinde yazmamizin sebebi url 'deki json formatindan swift'e cevirdigimiz verinin bir dizi icerisinde yer almasindan dolayidir. Yani liste icerisindeydi json verilerimiz biz de onu belirttik.
-    func downloadCurrenties(url: URL, completionHandler: @escaping (Result<[Crypto], NSError>) -> Void ) {
+    func downloadCurrenties(url: URL, completionHandler: @escaping (Result<[Crypto], CryptoError>) -> Void ) {
         // URLSession, (datatask ile) veri istegi yaptigimizda url'ye istek atar ve cevap geldikten sonra yapilacak islemleri burada yazmamiza olanak saglar
         URLSession.shared.dataTask(with: url) { data, response, error in
             // Response -cevap- geldikten sonra bu blok calisir
             
-        }
+            if let error = error {
+                completionHandler(.failure(.serverError))
+                // Eger hata yoksa bir data donmus demektir
+            } else if let data = data {
+                
+                // Do catch yapmamizin amaci bizim Crypto modelini yanlis yazmamiz olasiliginda uygulamayi cokmekten kurtarmak icin swift'in kendi uyarisi sonucu ekledik
+                do {
+                    // .decode sonrasinda parantez icerisinde neye gore decode edilecegi sorulur bize, biz de crypto dizisine gore decode edilecegini soyleriz
+                    let cryptoList = try? JSONDecoder().decode([Crypto].self, from: data)
+                    // cryptoList optionaldan kurtarmak icin if let yapisini kullanabiliriz
+                    if let cryptoList = cryptoList {
+                        completionHandler(.success(cryptoList))
+                        // eger bir onceki if let blogu calismazsa veri gelmesine ragmen ben bu datayi alamadim demektir, yine completionHandler cagirarak fail durumunu verip parsing'de sorun yasadigimizi belirtebiliriz
+                    } else {
+                        completionHandler(.failure(.parsingError))
+                    }
+                }
+            }
+        // UrlSession bitiminde resume etmezsek hicbir zaman calismaz unutmamamiz gereken bir durumdur
+        }.resume()
+        
     }
+    
+    
 }
